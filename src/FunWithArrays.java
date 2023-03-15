a562c7db726446e2341c8dc8ad3ca88eabcfefaf
NemuiLiz <nemui.liz@gmx.de>
2023-03-15 09:27:15 +0100

    Day 06 ArrayWrapper with Tests initial commit

---
+++ src/FunWithArrays.java
@@ -0,0 +1,16 @@
+public class Array {
+
+    public int[] numbers;
+
+    public Array(int[] numbers) {
+        this.numbers = new int[6];
+    }
+
+    public void addInt(int[] numbers) {
+
+    }
+
+    public void getArrayElement(int[] numbers) {
+        Array.get(int[] numbers, int index)
+    }
+}
